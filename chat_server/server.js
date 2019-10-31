7748395c51ad93918665e9df9e8dc468837a1138
Dominic Tremblay <dominictremblay24@gmail.com>
2019-10-31 02:02:25 -0400

    added web socket server

--- chat_server/server.js
+++ chat_server/server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const SocketServer = require('ws');
 const PORT = process.env.port || 3001;
 const app = express();
 const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const wss = new SocketServer.Server({ server });
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+
+  ws.on('message', message => {});
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
